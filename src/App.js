a07d22700776427fe890c75f95ef79a37e92c850
ZhyneAmenodin <amenodin.za28@s.msumain.edu.ph>
2022-08-19 15:06:27 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./PortfolioContainer/NavBar/NavBar";
 import Home from "./PortfolioContainer/Home/Home";
+import Skills from "./PortfolioContainer/Skills/Skills";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <Skills />
     </div>
   );
 }
---
+++ src/PortfolioContainer/Skills/Skills.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Skills.css";
+export default function Skills() {
+  return (
+    <div className="skill-container">
+      <div className="container-skill">
+        <div className="skill-header">
+          <h2>Skills</h2>
+          <p>Bellow are the list of my currents skills:</p>
+        </div>
+      </div>
+    </div>
+  );
+}
