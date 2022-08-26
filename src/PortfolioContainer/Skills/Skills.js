6bb22a590655dc34479fff15090d5dcf613e815e
ZhyneAmenodin <amenodin.za28@s.msumain.edu.ph>
2022-08-26 10:17:26 +0800

    skills page updates

--- src/PortfolioContainer/Skills/Skills.js
+++ src/PortfolioContainer/Skills/Skills.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./Skills.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 export default function Skills() {
   return (
     <div className="skill-container">
       <div className="container-skill">
         <div className="skill-header">
-          <h2>Skills</h2>
-          <p>Bellow are the list of my currents skills:</p>
+          <h1>Skills</h1>
+          <h4>Bellow are the list of my currents skills:</h4>
+
+          <Container>
+            <Col>
+              <div className="skill-list">
+                <div className="col">
+                  <Row>
+                    <h6>HTML: </h6>
+                    <span> Intermediate</span>
+                  </Row>
+                </div>
+                <div className="col">
+                  <h6>HTML: </h6>
+                </div>
+              </div>
+            </Col>
+          </Container>
         </div>
       </div>
     </div>
